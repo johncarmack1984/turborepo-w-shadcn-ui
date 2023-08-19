e44713027a3916c24fd0856f373ba804888b5913
John Carmack <johncarmack@me.com>
2023-08-19 12:29:36 -0700

    install shadcn and use it in an app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -26,18 +26,18 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4 pb-8 pt-16 sm:pt-24 lg:px-8">
         <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Web
-          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
-            Turborepo Example
+          <span className="block bg-gradient-to-r from-secondary to-primary bg-clip-text px-2 text-transparent">
+            shadcn/ui example
           </span>
         </h1>
         <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
           <Button />
         </div>
 
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-4 place-content-evenly">
+        <div className="mt-12 grid grid-cols-1 place-content-evenly gap-4 sm:grid-cols-3">
           {CARD_CONTENT.map((card) => (
             <Card key={card.title} {...card} />
           ))}
--- packages/ui/src/Card.tsx
+++ packages/ui/src/Card.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Card as ShadCard } from "@/components/ui/card";
 
 export const Card = ({
   title,
@@ -10,18 +11,13 @@ export const Card = ({
   href: string;
 }) => {
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href={href}
-      className="ui-group ui-mt-4 ui-rounded-lg ui-border ui-border-transparent ui-overflow-hidden ui-bg-origin-border ui-bg-gradient-to-r ui-from-brandred ui-to-brandblue ui-text-[#6b7280]"
-    >
-      <div className="ui-p-4 ui-bg-zinc-900 ui-h-full">
-        <p className="ui-inline-block ui-text-xl ui-text-white">{title}</p>
-        <div className="ui-text-xs ui-mt-4 group-hover:ui-underline">
-          {cta} →
+    <ShadCard className="group mt-4 overflow-hidden rounded-lg border border-transparent bg-gradient-to-r from-secondary to-primary bg-origin-border text-[#6b7280]">
+      <a target="_blank" rel="noopener noreferrer" href={href}>
+        <div className="h-full bg-zinc-900 p-4">
+          <p className="inline-block text-xl text-white">{title}</p>
+          <div className="mt-4 text-xs group-hover:underline">{cta} →</div>
         </div>
-      </div>
-    </a>
+      </a>
+    </ShadCard>
   );
 };
